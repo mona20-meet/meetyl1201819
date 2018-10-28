b3d180afb6b6276766a24fcbb4867c363a00d44f
mona20-meet <mona20@meet.mit.edu>
2018-10-28 18:14:07 +0200

    lab1 + lab2

---
+++ lab1.py
@@ -0,0 +1,9 @@
+number1 = 10
+print(number1)
+number2= number1/2
+print(number2)
+list1=[1,52343,3]
+for index in list1:
+	print(len(index + index))
+
+
---
+++ lab2.py
@@ -0,0 +1,13 @@
+y=[1,3,4,65,4]
+def thisthing (w):
+	x=[w[0],w[-1]]
+	return x
+thisthing(y)
+print(thisthing(y))
+
+
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+for index in a :
+	if index <= 6:
+		print(index)
